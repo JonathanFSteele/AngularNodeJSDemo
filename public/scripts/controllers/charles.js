2c0cb7c08a2164a3a39c2f58b2d33b0fe49b727f
Charles Choi <charlesachoi@gmail.com>
2017-02-11 14:42:24 -0700

    ITS FINISHEDDDDDDDDDDDD

--- public/scripts/controllers/charles.js
+++ public/scripts/controllers/charles.js
@@ -1,3 +1,17 @@
-scotchApp.controller('charlesController', function($scope) {
+scotchApp.controller('charlesController', function($scope, $http) {
   $scope.message = 'Look! I am an CHARLES page.';
+  $scope.Users = [
+    /*
+    {frname: "BOB", lastname: "BOBOBOB", },
+    {frname: "RICK", lastname: "BOBOBOB", },
+    {frname: "MORTY", lastname: "BOBOBOB", },
+    */
+  ];
+
+  $http.get("/api/users/list")
+  .then(function(response) {
+    console.log("response: ", response);
+    $scope.Users = response.data;
+  });
+
 });
