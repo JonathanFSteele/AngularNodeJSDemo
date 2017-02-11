5b67e77479971bb79bc02535aaf6c446bf0b64cc
Charles Choi <charlesachoi@gmail.com>
2017-02-11 14:20:27 -0700

    added charles page and link on main page

--- public/config.route.js
+++ public/config.route.js
@@ -8,6 +8,12 @@
 				controller  : 'homeController'
 			})
 
+			// route for the charles page
+			.when('/charles', {
+				templateUrl : 'views/charles.html',
+				controller  : 'charlesController'
+			})
+
 			// route for the about page
 			.when('/about', {
 				templateUrl : 'views/about.html',
---
+++ public/scripts/controllers/charles.js
@@ -0,0 +1,3 @@
+scotchApp.controller('charlesController', function($scope) {
+  $scope.message = 'Look! I am an CHARLES page.';
+});
